c4c8d14130b8f880b59d7a315c76d66222a2c35d
Renan de Carvalho Ribeiro <renan.ribeiro@mag-it.com.br>
2023-06-30 13:54:46 -0300

    Adicionado mais filtros e opção de limpar filtro

--- src/pages/admin/product/index.jsx
+++ src/pages/admin/product/index.jsx
@@ -3,12 +3,12 @@ import { useNavigate, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Container, Table, Form, Row, Col, Button } from "react-bootstrap";
 import useAlert from "../../../hooks/alert";
-import {FaPencilAlt, FaTrashAlt} from 'react-icons/fa';
+import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
 import usePagination from '../../../hooks/pagination';
-import {searchProductsRequest} from '../../../services/product';
+import { searchProductsRequest } from '../../../services/product';
 import Spinner from '../../../components/elements/spinner';
 import PaginationMenu from '../../../components/elements/paginationMenu';
-import {formatCurrency} from '../../../utils/helpers';
+import { formatCurrency } from '../../../utils/helpers';
 import Swal from "sweetalert2";
 
 export default function Products() {
@@ -25,15 +25,14 @@ export default function Products() {
 		setPage,
 		isLoading,
 		setFilter
-	} = usePagination(searchProductsRequest, {name: searchText});
+	} = usePagination(searchProductsRequest, { name: searchText });
 
 	const goToEdit = (id) => {
 		navigate(`${id}`);
 	};
 
 	const search = () => {
-		if(searchText === undefined || searchText.length === 0)
-		{
+		if (searchText === undefined || searchText.length === 0) {
 			Swal.fire({
 				icon: "error",
 				text: "Informe o nome do produto para pesquisar"
@@ -41,19 +40,24 @@ export default function Products() {
 			return;
 		}
 
-		setFilter({name: searchText});
+		setFilter({ name: searchText });
+	};
+	
+	const clearSearch = () => {
+		setSearchText("");
+		setFilter({});
 	};
 
 	return (
 		<Container>
 			<h3>{t("title")}</h3>
 			<Row>
-					<Col className="d-flex justify-content-end">
-						<Link to="add" className="btn btn-primary btn-tc-blue">
-							Adicionar
-						</Link>
-					</Col>
-				</Row>
+				<Col className="d-flex justify-content-end">
+					<Link to="add" className="btn btn-primary btn-tc-blue">
+						Adicionar
+					</Link>
+				</Col>
+			</Row>
 			<Row>
 				<Col>
 					<Table className="mt-5" striped bordered hover>
@@ -73,40 +77,51 @@ export default function Products() {
 											</Form.Group>
 										</Col>
 										<Col md={2}>
-											<Button onClick={() => search()}>
-												{t("labelSearch")}
-											</Button>
+											<Row className="mb-2">
+												<Col>
+													<Button onClick={() => search()}>
+														{t("labelSearch")}
+													</Button>
+												</Col>
+											</Row>
+											<Row>
+												<Col>
+													<Button variant="secondary" onClick={() => clearSearch()}>
+														{t("labelClearSearch")}
+													</Button>
+												</Col>
+											</Row>
 										</Col>
 									</Row>
 								</th>
 							</tr>
 							<tr>
-								<th style={{width: "50%"}}>Name</th>
-								<th style={{width: "50%"}}>Preço</th>
-								<th style={{width: "20%"}}></th>
+								<th style={{ width: "50%" }}>Name</th>
+								<th style={{ width: "50%" }}>Preço</th>
+								<th style={{ width: "20%" }}></th>
 							</tr>
 						</thead>
 						<tbody>
-						{isLoading ? (
-								<tr><td className="text-center" colSpan={5}><Spinner size={50}/></td></tr>
+							{isLoading ? (
+								<tr><td className="text-center" colSpan={5}><Spinner size={50} /></td></tr>
 							) : (
 								products?.map((product) => (
-							<tr>
-								<td>{product.name}</td>
-								<td>{formatCurrency(product.price)}</td>
-								<td className="text-center">
-									<Button 
-										variant="warning" 
-										className="ms-1 me-1"
-										onClick={() => goToEdit(product.id)}>
-										<FaPencilAlt/>
-									</Button>
-									{/* <Button variant="danger" className="ms-1 me-1">
+									<tr>
+										<td>{product.name}</td>
+										<td>{formatCurrency(product.price)}</td>
+										<td className="text-center">
+											<Button
+												variant="warning"
+												className="ms-1 me-1"
+												onClick={() => goToEdit(product.id)}>
+												<FaPencilAlt />
+											</Button>
+											{/* <Button variant="danger" className="ms-1 me-1">
 										<FaTrashAlt/>
 									</Button> */}
-								</td>
-							</tr>
-							))
+										</td>
+									</tr>
+								))
 							)}
 						</tbody>
 					</Table>
--- src/pages/admin/supplierProduct/index.jsx
+++ src/pages/admin/supplierProduct/index.jsx
@@ -3,9 +3,9 @@ import { useNavigate, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { Container, Table, Form, Row, Col, Button } from "react-bootstrap";
 import useAlert from "../../../hooks/alert";
-import {FaPencilAlt, FaTrashAlt} from 'react-icons/fa';
+import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
 import usePagination from '../../../hooks/pagination';
-import {searchSupplierProductsRequest} from '../../../services/supplierProduct';
+import { searchSupplierProductsRequest } from '../../../services/supplierProduct';
 import Spinner from '../../../components/elements/spinner';
 import PaginationMenu from '../../../components/elements/paginationMenu';
 import Swal from "sweetalert2";
@@ -14,6 +14,7 @@ export default function SupplierProducts() {
 	const { t } = useTranslation("supplierProducts");
 	const navigate = useNavigate();
 	const [searchText, setSearchText] = useState("");
+	const [searchCode, setSearchCode] = useState("");
 	const {
 		data: products,
 		pageIndex,
@@ -23,23 +24,29 @@ export default function SupplierProducts() {
 		setPage,
 		isLoading,
 		setFilter
-	} = usePagination(searchSupplierProductsRequest, {name: searchText});
+	} = usePagination(searchSupplierProductsRequest, { name: searchText, externalCode: searchCode });
 
 	const goToEdit = (id) => {
 		navigate(`${id}`);
 	};
 
 	const search = () => {
-		if(searchText === undefined || searchText.length === 0)
-		{
+		if ((searchText === undefined || searchText.length === 0)
+			&& (searchCode === undefined || searchCode.length === 0)) {
 			Swal.fire({
 				icon: "error",
-				text: "Informe o nome do produto para pesquisar"
+				text: "Informe o nome ou código externo do produto para pesquisar"
 			});
 			return;
 		}
 
-		setFilter({name: searchText});
+		setFilter({ name: searchText, externalCode: searchCode });
+	};
+
+	const clearSearch = () => {
+		setSearchText("");
+		setSearchCode("");
+		setFilter({});
 	};
 
 	return (
@@ -53,54 +60,90 @@ export default function SupplierProducts() {
 					<Table className="mt-5" striped bordered hover>
 						<thead>
 							<tr>
-								<th colSpan={3}>
+								<th colSpan={4}>
 									<Row>
 										<Col md={10}>
-											<Form.Group as={Row} className="mb-2">
+											<Row>
 												<Col>
-													<Form.Control
-														type="text"
-														value={searchText}
-														onChange={(e) => setSearchText(e.target.value)}
-													/>
+													<Form.Group as={Row} className="mb-2">
+														<Form.Label column md={3} className="d-flex justify-content-end">
+															Nome:
+														</Form.Label>
+														<Col>
+															<Form.Control
+																type="text"
+																value={searchText}
+																onChange={(e) => setSearchText(e.target.value)}
+															/>
+														</Col>
+													</Form.Group>
 												</Col>
-											</Form.Group>
+											</Row>
+											<Row>
+												<Col>
+													<Form.Group as={Row} className="mb-2">
+														<Form.Label column md={3} className="d-flex justify-content-end">
+															Código Externo:
+														</Form.Label>
+														<Col>
+															<Form.Control
+																type="text"
+																value={searchCode}
+																onChange={(e) => setSearchCode(e.target.value)}
+															/>
+														</Col>
+													</Form.Group>
+												</Col>
+											</Row>
 										</Col>
 										<Col md={2}>
-											<Button onClick={() => search()}>
-												{t("labelSearch")}
-											</Button>
+											<Row className="mb-2">
+												<Col>
+													<Button onClick={() => search()}>
+														{t("labelSearch")}
+													</Button>
+												</Col>
+											</Row>
+											<Row>
+												<Col>
+													<Button variant="secondary" onClick={() => clearSearch()}>
+														{t("labelClearSearch")}
+													</Button>
+												</Col>
+											</Row>
 										</Col>
 									</Row>
 								</th>
 							</tr>
 							<tr>
-								<th style={{width: "50%"}}>Name</th>
-								<th style={{width: "50%"}}>Tipo</th>
-								<th style={{width: "20%"}}></th>
+								<th style={{ width: "40%" }}>Name</th>
+								<th style={{ width: "20%" }}>Código Externo</th>
+								<th style={{ width: "20%" }}>Tipo</th>
+								<th style={{ width: "20%" }}></th>
 							</tr>
 						</thead>
 						<tbody>
-						{isLoading ? (
-								<tr><td className="text-center" colSpan={5}><Spinner size={50}/></td></tr>
+							{isLoading ? (
+								<tr><td className="text-center" colSpan={5}><Spinner size={50} /></td></tr>
 							) : (
 								products?.map((product) => (
-							<tr>
-								<td>{product.name}</td>
-								<td>{product.productType.name}</td>
-								<td className="text-center">
-									<Button 
-										variant="warning" 
-										className="ms-1 me-1"
-										onClick={() => goToEdit(product.id)}>
-										<FaPencilAlt/>
-									</Button>
-									{/* <Button variant="danger" className="ms-1 me-1">
+									<tr>
+										<td>{product.name}</td>
+										<td>{product.externalCode}</td>
+										<td>{product.productType.name}</td>
+										<td className="text-center">
+											<Button
+												variant="warning"
+												className="ms-1 me-1"
+												onClick={() => goToEdit(product.id)}>
+												<FaPencilAlt />
+											</Button>
+											{/* <Button variant="danger" className="ms-1 me-1">
 										<FaTrashAlt/>
 									</Button> */}
-								</td>
-							</tr>
-							))
+										</td>
+									</tr>
+								))
 							)}
 						</tbody>
 					</Table>
